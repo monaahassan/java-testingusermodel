1bcac62411e076569fbf0ebe4bca1360dc4987f3
Mona Hassan <monamia@Monas-MacBook-Pro.local>
2021-02-24 21:44:26 -0600

    Starting Project

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/UserModelApplicationTesting.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/UserModelApplicationTesting.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Main class to start the application.
  */
+//Turn off
 // @EnableJpaAuditing
 @SpringBootApplication
 public class UserModelApplicationTesting
